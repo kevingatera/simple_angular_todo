aebd133b74cedfbbe330fcf86763f6fca24dbf7c
Lowell Mower <lowell.m.mower@gmail.com>
2015-08-28 12:17:17 -0400

    form and submit function

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ var myApp = angular.module('ToDo', []);
 myApp.controller('todoController', ['$scope', function($scope){
   $scope.todos = [
     {'title':'Test Item', 'done': false}
-  ]
+  ];
+  $scope.addItem = function(){
+
+  }
 }])
